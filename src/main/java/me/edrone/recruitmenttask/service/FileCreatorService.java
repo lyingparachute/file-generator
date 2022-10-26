decb25e580be73502199aa4a9e66f1b24e4ea66c
lyingparachute <igor1501@o2.pl>
2022-10-26 16:20:59 +0200

    Created new method of getting possible combinations, added explanation

--- src/main/java/me/edrone/recruitmenttask/service/FileCreatorService.java
+++ src/main/java/me/edrone/recruitmenttask/service/FileCreatorService.java
@@ -1,18 +1,26 @@
 package me.edrone.recruitmenttask.service;
-
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.stream.IntStream;
 
 
 @Service
 public class FileCreatorService {
 
-    public Integer getNumberOfPossibleCombinations(char[] chars, int lengthOfResultString) {
+    // Number of possible combinations without repeating letters
+    // for example: from String "abc" -> "aaa" is NOT possible
+    public Integer getNumberOfPossibleCombinations(char[] chars, int targetStringLength) {
         int numOfChars = chars.length;
-        int subtractionResult = Math.subtractExact(numOfChars, lengthOfResultString);
+        int subtractionResult = Math.subtractExact(numOfChars, targetStringLength);
         return Math.floorDiv(calculateFactorial(numOfChars),calculateFactorial(subtractionResult));
+    }
 
+    // Number of possible combinations with possibility of repeating letters
+    // for example: from String "abc" -> "aaa" is possible
+    public Integer getNumberOfPossibleCombinationsVariation(char[] chars, int targetStringLength) {
+        return (int) Math.pow(chars.length, targetStringLength);
     }
 
     public Integer calculateFactorial(int basis) {
