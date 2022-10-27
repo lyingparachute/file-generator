1000a3e78942731521aea8b474aabc2d892042fb
lyingparachute <igor1501@o2.pl>
2022-10-27 19:00:57 +0200

    Add repository methods

--- src/main/java/me/edrone/recruitmenttask/repository/FileRepository.java
+++ src/main/java/me/edrone/recruitmenttask/repository/FileRepository.java
@@ -1,7 +1,22 @@
 package me.edrone.recruitmenttask.repository;
 
 import me.edrone.recruitmenttask.entity.FileEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Set;
 
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
+//    Set<String> findByIdAnd
+
+
+    @Query("select f from FileEntity f left join fetch f.setOfStrings")
+    List<FileEntity> findAllWithStringSet();
+
+
+    @Override
+    @EntityGraph(attributePaths = "setOfStrings")
+    List<FileEntity> findAll();
 }
