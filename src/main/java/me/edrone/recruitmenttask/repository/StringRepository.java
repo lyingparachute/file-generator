c658ff66243c6b48f7544736df7d046c35dcd5e9
lyingparachute <igor1501@o2.pl>
2022-10-27 19:00:03 +0200

    deleted unnecessary classes

--- src/main/java/me/edrone/recruitmenttask/repository/StringRepository.java
+++
@@ -1,9 +0,0 @@
-package me.edrone.recruitmenttask.repository;
-
-import me.edrone.recruitmenttask.entity.StringEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StringRepository extends JpaRepository<StringEntity, Long> {
-}
