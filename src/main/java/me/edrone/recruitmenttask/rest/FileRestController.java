1b21744ca70fcbce0c05b55d14dca7b09eda791f
lyingparachute <igor1501@o2.pl>
2022-10-27 18:12:38 +0200

    Add create method in controller

--- src/main/java/me/edrone/recruitmenttask/rest/FileRestController.java
+++ src/main/java/me/edrone/recruitmenttask/rest/FileRestController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/files")
 public class FileRestController {
@@ -23,15 +25,15 @@ public class FileRestController {
     }
 
     @GetMapping()
-    public ResponseEntity<FileDto> getCurrentJobsResults() {
-        return null;
+    public ResponseEntity<List<FileDto>> getCurrentJobsResults() {
+        return ResponseEntity.ok(fileService.getAllCurrentJobs());
     }
 
     @PostMapping("create")
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<FileDto> createJob(@RequestBody FileDto fileDto) {
-
-
-        return null;
+        FileDto created = fileService.create(fileDto);
+        return created != null ?
+                ResponseEntity.status(HttpStatus.CREATED).body(created) :
+                ResponseEntity.badRequest().build();
     }
 }
