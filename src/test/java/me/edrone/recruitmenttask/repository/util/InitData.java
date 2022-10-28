3f27eeecf60b54f7f21eefecae2248c2e464ee4a
lyingparachute <igor1501@o2.pl>
2022-10-28 21:38:03 +0200

    Move reoccuring methods to InitData

--- src/test/java/me/edrone/recruitmenttask/repository/util/InitData.java
+++ src/test/java/me/edrone/recruitmenttask/repository/util/InitData.java
@@ -1,9 +1,44 @@
 package me.edrone.recruitmenttask.repository.util;
 
+import me.edrone.recruitmenttask.dto.FileDto;
+import me.edrone.recruitmenttask.repository.FileRepository;
+import me.edrone.recruitmenttask.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.io.File;
 
 @Component
 public class InitData {
+
+    public static final String FILE_NAME = "src/main/resources/files/1.txt";
+
+    @Autowired
+    private FileRepository fileRepository;
+
+    @Autowired
+    private FileService fileService;
+
+    @Transactional
+    public void cleanUp(){
+        fileRepository.deleteAll();
+    }
+
+    public void cleanUpFiles() {
+        new File(FILE_NAME).delete();
+    }
+
+    public FileDto createFileDto () {
+        FileDto fileDto = new FileDto();
+        fileDto.setAvailableChars("abcde");
+        fileDto.setMinLengthOfTargetString(2);
+        fileDto.setMaxLengthOfTargetString(3);
+        fileDto.setNumberOfTargetStrings(5);
+        return fileService.create(fileDto);
+    }
+
+
 
 
 }
---
+++ src/test/java/me/edrone/recruitmenttask/service/LocalFileServiceTest.java
@@ -0,0 +1,59 @@
+package me.edrone.recruitmenttask.service;
+
+import me.edrone.recruitmenttask.dto.FileDto;
+import me.edrone.recruitmenttask.repository.FileRepository;
+import me.edrone.recruitmenttask.repository.util.InitData;
+import me.edrone.recruitmenttask.service.FileService;
+import me.edrone.recruitmenttask.service.LocalFileService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LocalFileServiceTest {
+
+    @Autowired
+    LocalFileService localFileService;
+
+    @Autowired
+    FileService fileService;
+
+    @Autowired
+    InitData initData;
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @AfterEach
+    @BeforeEach
+    public void cleanUp() {
+        initData.cleanUpFiles();
+        initData.cleanUp();
+    }
+
+    @Test
+    void createLocalFile_givenFileEntity() {
+        //given
+        FileDto fileDto = initData.createFileDto();
+
+        //when
+        BufferedWriter created = localFileService.createFile(fileDto);
+
+        //then
+        assertThat(created).isNotNull();
+        assertTrue(Files.exists(Path.of(InitData.FILE_NAME)));
+        assertThat(new File(InitData.FILE_NAME)).isFile();
+
+    }
+}
